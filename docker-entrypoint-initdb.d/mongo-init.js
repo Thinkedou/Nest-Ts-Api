3a2f685e8751b1a71d069d0539ecd240c0fd54fe
Thinkedou <r.enguehard@gmail.com>
2023-09-12 09:21:19 +0200

    trying to quiet mongosh (failed)

--- docker-entrypoint-initdb.d/mongo-init.js
+++ docker-entrypoint-initdb.d/mongo-init.js
@@ -1,7 +1,32 @@
-print('Start #################################################################');
+print(' START injection mode ~~~~~~~~~~~~~ ');
+
+const INIT_DATASET = [
+    {
+        title: '1984',
+        originalTitle: 'Nineteen Eighty-Four',
+        author: { firstName: 'George', lastName: 'Orwell' },
+        publicationYear: 1950,
+        genre: 'Science Fiction',
+        coverUri: 'https://media.senscritique.com/media/000004731893/300/1984.jpg',
+        recap: ''
+    },
+    {
+        title: 'Dune',
+        originalTitle: 'Dune',
+        author:{
+            firstName:"Frank",
+            lastName :"Herbert"
+        },
+        publicationYear: 1965,
+        genre: 'Science Fiction',
+        coverUri: 'https://media.senscritique.com/media/000020354712/300/dune.jpg',
+        recap: ''
+      }
+]
+
 
 db = db.getSiblingDB('bookstore');
 db.createCollection('books');
-db.books.insert( { name: "biclou", price: 1115 } )
+db.books.insertMany(INIT_DATASET)
 
-print('END #################################################################');
+print(' END injection mode ~~~~~~~~~~~~~ ');
